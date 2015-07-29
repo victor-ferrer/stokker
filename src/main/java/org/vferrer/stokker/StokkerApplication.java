199726c74f912821d7524b45dfa009c546955b8a
Victor Ferrer <victor.ferrer.posa@ericsson.com>
2015-07-29 11:27:02 +0200

    Added: - Simple Docker Image + maven plugin - Dummy Hello World page (for testing) - Actuator dependency - Additional .gitignore entries

--- src/main/java/org/vferrer/stokker/StokkerApplication.java
+++ src/main/java/org/vferrer/stokker/StokkerApplication.java
@@ -3,7 +3,10 @@ package org.vferrer.stokker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @SpringBootApplication
 @ImportResource("classpath:/META-INF/feederFlow.xml")
 public class StokkerApplication {
@@ -11,4 +14,10 @@ public class StokkerApplication {
     public static void main(String[] args) {
         SpringApplication.run(StokkerApplication.class, args);
     }
+    
+    @RequestMapping("/")
+    public String home() {
+    	return "Hello World";
+    }
+    
 }
