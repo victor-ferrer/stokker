331f71411ecc6d3511e40535f6bf8c57d3713f3f
efevict <victor.ferrer.posa@ericsson.com>
2015-10-07 12:42:23 +0200

    Added sample stocks and portfolio SQL data

---
+++ portfolioManager/src/main/resources/data.sql
@@ -0,0 +1,17 @@
+insert into portfolio (portfolio_id, label, incorporation_date, currency) values (1, 'Dividend portfolio', '2015-04-01','EUR');
+
+insert into stock (stock_id, ticker, market, need_market_suffix, label, currency) values (1, 'MAP', 'MC', 1, 'Mapfre seguros', 'EUR');
+insert into stock (stock_id, ticker, market, need_market_suffix, label, currency) values (2, 'SAN', 'MC', 1, 'Banco Santander', 'EUR');
+insert into stock (stock_id, ticker, market, need_market_suffix, label, currency) values (3, 'REP', 'MC', 1, 'Repsol', 'EUR');
+insert into stock (stock_id, ticker, market, need_market_suffix, label, currency) values (4, 'ENG', 'MC', 1, 'Enagás', 'EUR');
+insert into stock (stock_id, ticker, market, need_market_suffix, label, currency) values (5, 'ABE', 'MC', 1, 'Abertis Infraestructuras', 'EUR');
+insert into stock (stock_id, ticker, market, need_market_suffix, label, currency) values (6, 'TEF', 'MC', 1, 'Telefónica', 'EUR');
+insert into stock (stock_id, ticker, market, need_market_suffix, label, currency) values (7, 'IBE', 'MC', 1, 'Iberdrola', 'EUR');
+insert into stock (stock_id, ticker, market, need_market_suffix, label, currency) values (8, 'GAS', 'MC', 1, 'Gas Natural', 'EUR');
+insert into stock (stock_id, ticker, market, need_market_suffix, label, currency) values (9, 'REE', 'MC', 1, 'Red Eléctrica Corporación', 'EUR');
+insert into stock (stock_id, ticker, market, need_market_suffix, label, currency) values (10, 'ELE', 'MC', 1, 'Endesa', 'EUR');
+insert into stock (stock_id, ticker, market, need_market_suffix, label, currency) values (11, 'T', 'NYSE', 0, 'AT&T', 'USD');
+insert into stock (stock_id, ticker, market, need_market_suffix, label, currency) values (12, 'OHI', 'NYSE', 0, 'Omega Healthcare Investors', 'USD');
+insert into stock (stock_id, ticker, market, need_market_suffix, label, currency) values (13, 'BME', 'MC', 1, 'Bolsas y Mercados', 'EUR');
+insert into stock (stock_id, ticker, market, need_market_suffix, label, currency) values (14, 'ACS', 'MC', 1, 'ACS Contrucciones', 'EUR');
+
