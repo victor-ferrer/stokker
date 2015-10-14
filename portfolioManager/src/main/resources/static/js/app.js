fd60ee2904b1a0c40c014721a1f2a6649ee5d211
efevict <victor.ferrer.posa@ericsson.com>
2015-10-14 22:25:11 +0200

    Added refresh, filtering and querying on the kibana iframe

--- portfolioManager/src/main/resources/static/js/app.js
+++ portfolioManager/src/main/resources/static/js/app.js
@@ -36,23 +36,56 @@ angular.module('portfolio_manager', [ 'ngRoute' ])
 		    angular.forEach($scope.positions, function(position) {
 		    	  $http.get(position._links.stock.href).success(function(stockdata){
 		    		  position.stock = stockdata;
+		    		  position.currentPrice = 0;
 		    	  })
 		    });
 		    
 		  })
 })
-.controller('stocksController', function($scope, $http, $sce){
+.controller('stocksController', function($scope, $http, $sce, $document, $routeParams){
 	  $http.get('/stocks/').success(function(data) {
 		    $scope.stocks = data._embedded.stocks;
 		  });
 
-	  $scope.query = 'MAP.MC';
-	  $scope.dahsboardURL = $sce.trustAsResourceUrl("http://192.168.59.103:5601/#/dashboard/Full_dashboard?embed&_g=(refreshInterval:(display:Off,pause:!f,section:0,value:0),time:(from:now-1y,mode:quick,to:now))&_a=(filters:!(),panels:!((col:1,id:Stock-Values,row:1,size_x:9,size_y:6,type:visualization),(col:1,id:Stock-volume,row:7,size_x:9,size_y:2,type:visualization)),query:(query_string:(analyze_wildcard:!t,query:'" + $scope.query + "')),title:Full_dashboard)");
 	  
-	  $scope.refreshIframe = function() {
-		  $scope.dahsboardURL = $sce.trustAsResourceUrl("http://192.168.59.103:5601/#/dashboard/Full_dashboard?embed&_g=(refreshInterval:(display:Off,pause:!f,section:0,value:0),time:(from:now-1y,mode:quick,to:now))&_a=(filters:!(),panels:!((col:1,id:Stock-Values,row:1,size_x:9,size_y:6,type:visualization),(col:1,id:Stock-volume,row:7,size_x:9,size_y:2,type:visualization)),query:(query_string:(analyze_wildcard:!t,query:'" + $scope.query + "')),title:Full_dashboard)");
+	  $scope.init = function () {
+		  	$scope.query = '*';
+		  	$scope.tab = [{ "refresh" : false}];
+		  	
+		  	$http.get($routeParams.stockInfo).success(function(data){
+		  		$scope.targetStock = data;  
+		  		$scope.query = $scope.targetStock.ticker + "." + $scope.targetStock.market;
+		  		
+		  		// WARNING This should be customized with the URL created by Kibana when exporting the dashboard
+		  		$scope.dahsboardURL = $sce.trustAsResourceUrl("http://localhost:5601/#/dashboard/Full_dashboard?embed&_g=(refreshInterval:(display:Off,pause:!f,section:0,value:0),time:(from:now-1y,mode:quick,to:now))&_a=(filters:!(),panels:!((col:1,id:Stock-Values,row:1,size_x:12,size_y:4,type:visualization),(col:1,id:Stock-volume,row:7,size_x:12,size_y:2,type:visualization)),query:(query_string:(analyze_wildcard:!t,query:'stock%3D!'"+ $scope.query +  "!'')),title:Full_dashboard)");		  
+		  	});
+		  	
+		  	$scope.timeframes = [{ "label" : "Last 2 years", "code" : "now-2y"},
+			                       { "label" : "Last year", "code" : "now-1y"},
+			                       { "label" : "Last 6 months", "code" : "now-6M"},
+			                       { "label" : "Last month", "code" : "now-1M"},
+			                       { "label" : "Last 2 weeks", "code" : "now-2w"},
+			                       { "label" : "Last week", "code" : "now-1w"} ];
+			  
+			  $scope.targetTimeFrame = [{ "label" : "Last year", "code" : "now-1y"}];
 		}
+	  $scope.init();
+	  
+	  
+	  $scope.updateIframe = function() {
 		  
+		  if ($scope.targetStock.needMarketSuffix){
+			  $scope.query = $scope.targetStock.ticker + "." + $scope.targetStock.market;
+		  }
+		  else {
+			  $scope.query = $scope.targetStock.ticker;
+		  }
+		  
+		  $scope.dahsboardURL = $sce.trustAsResourceUrl("http://localhost:5601/#/dashboard/Full_dashboard?embed&_g=(refreshInterval:(display:Off,pause:!f,section:0,value:0),time:(from:" + $scope.targetTimeFrame.code +  ",mode:quick,to:now))&_a=(filters:!(),panels:!((col:1,id:Stock-Values,row:1,size_x:12,size_y:4,type:visualization),(col:1,id:Stock-volume,row:7,size_x:12,size_y:2,type:visualization)),query:(query_string:(analyze_wildcard:!t,query:'stock%3D!'"+ $scope.query +  "!'')),title:Full_dashboard)");
+		  $scope.tab.refresh=true;
+		}
+	  
+	  
 		  
 })
 .controller('navigation',
@@ -103,4 +136,24 @@ angular.module('portfolio_manager', [ 'ngRoute' ])
 	}
 
 
-});
+})
+.directive('refreshable', [function () {
+    return {
+        restrict: 'A',
+        scope: {
+            refresh: "=refreshable"
+        },
+        link: function (scope, element, attr) {
+            var refreshMe = function () {
+                element.attr('src', element.attr('src'));
+            };
+
+            scope.$watch('refresh', function (newVal, oldVal) {
+                if (scope.refresh) {
+                    scope.refresh = false;
+                    refreshMe();
+                }
+            });
+        }
+    };
+}]);
