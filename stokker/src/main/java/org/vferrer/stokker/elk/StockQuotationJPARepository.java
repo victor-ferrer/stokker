0bef057558b50725cf2fde0fb49ea1864c098774
victor-ferrer <vferrer2005@gmail.com>
2015-11-01 20:51:56 +0100

    Fixed param in stock quotation repo

--- stokker/src/main/java/org/vferrer/stokker/elk/StockQuotationJPARepository.java
+++ stokker/src/main/java/org/vferrer/stokker/elk/StockQuotationJPARepository.java
@@ -3,13 +3,14 @@ package org.vferrer.stokker.elk;
 import java.util.List;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Component;
 
 @Component
 public interface StockQuotationJPARepository extends PagingAndSortingRepository<StockQuotationJPA, Long> 
 {
 	
-	public List<StockQuotationJPA> findValueByStock(String stock);
+	public List<StockQuotationJPA> findValueByStock(@Param("ticker") String ticker);
 
 	
 }
