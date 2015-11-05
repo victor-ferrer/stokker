604533d32d13adda5e33f707147fab438e76fb7c
Victor Ferrer <victor.ferrer.posa@ericsson.com>
2015-11-05 15:20:33 +0100

    Added eureka support

--- stokker/src/main/java/org/vferrer/stokker/StokkerApplication.java
+++ stokker/src/main/java/org/vferrer/stokker/StokkerApplication.java
@@ -2,11 +2,12 @@ package org.vferrer.stokker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @ImportResource(value = {"classpath:/META-INF/feederFlow.xml","classpath:/META-INF/historicalQuotations.xml","classpath:/META-INF/liveQuotations.xml"})
-
+@EnableEurekaClient
 // https://jira.spring.io/browse/DATAES-137
 // This bug prevents Spring Data REST to work with ElasticSearchRepositories
 // Fix Version is 1.3.0 GA (Gosling)
